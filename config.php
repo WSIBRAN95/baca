bad9436f11dcdad764be15fa198e5f0ce5a07213
WSIBRAN95 <49523383+WSIBRAN95@users.noreply.github.com>
2019-07-03 19:30:15 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,14 @@
+<?php
+
+
+$uid="Facebook_2299730140119467";
+$raw_uid="6c1801be-8075-4bd5-b99e-580b0f1009bd";
+$user_name="Wahyu+Sibran";
+$invite_code="FAX22P0VK7";
+$user_token="AbzdKydge1Vp0aK_";
+$own_mac="02%3A00%3A00%3A00%3A00%3A00";
+$android_id="fb237fa198b8c399";
+$device_type="Xiaomi++Redmi+Note+4";
+
+
+?>
